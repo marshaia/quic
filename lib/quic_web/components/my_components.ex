54ce93329c93e7262098c3ce24a7807b40df77a8
marshaia <joana15501@gmail.com>
2024-06-03 16:01:54 +0100

    minor interface improvements

--- lib/quic_web/components/my_components.ex
+++ lib/quic_web/components/my_components.ex
@@ -97,7 +97,7 @@ defmodule QuicWeb.MyComponents do
       data-cancel={JS.exec(@on_cancel, "phx-remove")}
       class="relative z-50 hidden"
     >
-      <div id={"#{@id}-bg"} class="fixed inset-0 transition-opacity bg-zinc-50/90 dark:bg-slate-900/90" aria-hidden="true" />
+      <div id={"#{@id}-bg"} class="fixed inset-0 transition-opacity bg-zinc-400/90 dark:bg-slate-900/90" aria-hidden="true" />
       <div
         class="fixed inset-0 overflow-y-auto"
         aria-labelledby={"#{@id}-title"}
